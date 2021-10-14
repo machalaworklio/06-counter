9cb1747f439b27e42729639b130627f77185f579
filipmachala88 <filipmachala88@gmail.com>
2021-10-14 14:56:38 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>Counter</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  setup() {
+    // Code
+  },
+});
+</script>
+
+<style lang="scss" module>
+// Styles
+</style>
